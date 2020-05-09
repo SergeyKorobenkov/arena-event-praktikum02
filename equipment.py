5c24463743cb9a950f43a7def6569cd9ce701cd0
Odintsov Timur <timodin@yandex.ru>
2020-05-09 19:08:07 +0300

    equipment: made blanc classes for the future

--- equipment.py
+++ equipment.py
@@ -5,3 +5,19 @@ class Equipment():
         self.attack = attack
         self.hp = hp
 
+
+class Headdress(Equipment):
+    def __init__(self, name, protection, attack, hp):
+        super().__init__(name, protection, attack, hp)
+
+
+class Armor(Equipment):
+    pass
+
+
+class Legs(Equipment):
+    pass
+
+
+class Weapon(Equipment):
+    pass
