39ffbcc51a88d182d31fcaa173ec72abce3a6076
Odintsov Timur <timodin@yandex.ru>
2020-05-09 19:06:56 +0300

    equipment: made Equipment() class

---
+++ equipment.py
@@ -0,0 +1,7 @@
+class Equipment():
+    def __init__(self, name, protection, attack, hp):
+        self.name = name
+        self.protection = protection
+        self.attack = attack
+        self.hp = hp
+
