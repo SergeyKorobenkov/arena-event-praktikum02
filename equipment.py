32934e83027881d392a26a99e858b3527d355c2c
Odintsov Timur <timodin@yandex.ru>
2020-05-10 03:57:53 +0300

    equipment: add docstrings to class and its method

--- equipment.py
+++ equipment.py
@@ -1,23 +1,25 @@
 class Equipment():
+    '''
+    A class used to represent different Equipment types.
+
+    Attributes:
+        name (str): equipment name.
+        protection (int): equipment protection.
+        attack (int): equipment attack.
+        hp (int): equipment hp.
+    '''
+
     def __init__(self, name, protection, attack, hp):
+        '''
+        The constructor for Equipment class.
+
+        Parameters:
+            name (str): equipment name.
+            protection (int): equipment protection.
+            attack (int): equipment attack.
+            hp (int): equipment hp.
+        '''
         self.name = name
         self.protection = protection
         self.attack = attack
         self.hp = hp
-
-
-class Headdress(Equipment):
-    def __init__(self, name, protection, attack, hp):
-        super().__init__(name, protection, attack, hp)
-
-
-class Armor(Equipment):
-    pass
-
-
-class Legs(Equipment):
-    pass
-
-
-class Weapon(Equipment):
-    pass
